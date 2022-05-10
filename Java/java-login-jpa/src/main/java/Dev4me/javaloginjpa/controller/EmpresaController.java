9cd6c52f535c130dd9d64fd8fe04a25055e704aa
flamauricio <fla.mauricio.valerio17@gmail.com>
2022-05-10 18:15:52 -0300

    Created method sending email enterprise

--- Java/java-login-jpa/src/main/java/Dev4me/javaloginjpa/controller/EmpresaController.java
+++ Java/java-login-jpa/src/main/java/Dev4me/javaloginjpa/controller/EmpresaController.java
@@ -1,8 +1,12 @@
 package Dev4me.javaloginjpa.controller;
 
+import Dev4me.javaloginjpa.entity.Email;
 import Dev4me.javaloginjpa.entity.Empresa;
 import Dev4me.javaloginjpa.entity.Usuario;
+import Dev4me.javaloginjpa.enums.StatusEmail;
+import Dev4me.javaloginjpa.repository.EmailRepository;
 import Dev4me.javaloginjpa.repository.EmpresaRepository;
+import Dev4me.javaloginjpa.repository.UsuarioRepository;
 import Dev4me.javaloginjpa.request.EmpresaSenhaRequest;
 import Dev4me.javaloginjpa.request.UsuarioSenhaRequest;
 import Dev4me.javaloginjpa.response.EmpresaAutenticacaoResponse;
@@ -12,11 +16,18 @@ import Dev4me.javaloginjpa.response.UsuarioSimplesResponse;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.validation.Valid;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -26,13 +37,48 @@ public class EmpresaController {
     @Autowired
     private EmpresaRepository repository;
 
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    private JavaMailSender emailSender;
+
     //Método pra cadastro de Empresa;
     @ApiResponses({@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))})
     @PostMapping
     @CrossOrigin
     public ResponseEntity postEmpresa(@RequestBody @Valid Empresa novaEmpresa) {
         repository.save(novaEmpresa);
+        String email = novaEmpresa.getEmail();
+        String uri = "http://localhost:8080/usuarios/sending-email/" + email;
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.postForObject(uri, novaEmpresa, String.class);
         return ResponseEntity.status(201).build();
+    }
+
+    // Enviar email
+    @ApiResponses({@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json"))})
+    @PostMapping("/sending-email/{email}")
+    @CrossOrigin
+    public ResponseEntity<Email> sendingEmail(@RequestBody @Valid Email emailDto, @PathVariable String email) {
+        Email emailModel = new Email();
+        emailModel.setSendDateEmail(LocalDateTime.now());
+        emailModel.setEmailTo(email);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(emailModel.getEmailFrom());
+            message.setTo(emailModel.getEmailTo());
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message);
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        } catch (MailException e) {
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            emailRepository.save(emailModel);
+        }
+        BeanUtils.copyProperties(emailDto, emailModel);
+        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
     }
 
     // GET da empresa sem senha
