36b3001ae7a2458e7d0d5a9afd8b3d60a30cc7b4
gui0103 <guilherme.ccosta@bandtec.com.br>
2022-05-21 16:45:31 -0300

    page PerfilUsuario React done

--- Site/src/pages/PerfilUsuario.jsx
+++ Site/src/pages/PerfilUsuario.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import HeaderLogado from "../components/HeaderLogado";
+import HeaderLogadoDois from "../components/HeaderLogadoDois";
 import api from "../api";
 import alertImage from "../img/warning.png"
 
@@ -7,13 +7,13 @@ function PerfilUsuario() {
 
     return(
         <>
-        <HeaderLogado />
+        <HeaderLogadoDois />
         <div className="sidebar-user">
             <div className="sidebar-container-user">
                 <div className="div-image-alert">
                     <img className="image-alert" src={alertImage} />
                 </div>
-                <p className="bigTitle">Configurações</p>
+                <p className="bigTitleUserProfile">Configurações</p>
                 <p className="title-user">Editar informações</p>
                 <p className="title-user">Mudar senha</p>
                 <p className="title-user">Sair</p>
@@ -21,11 +21,71 @@ function PerfilUsuario() {
         </div>
 
         <div className="main-div-user">
-            <div className="divSpaceBetween4">
+
+            <div className="div-image-alert">
+                    <img className="image-alert-invisible" src={alertImage} />
+            </div>
+
+            <div className="divSpaceBetween2">
                 <div className="div-input-user-small">
-                    <p className="bigTitle">Nome</p>
+                    <p className="bigTitleUserProfile">Nome</p>
                     <input className="input-user-small" type="text" />
                 </div>
+
+                <div className="div-input-user-small">
+                    <p className="bigTitleUserProfile">Localização</p>
+                    <input className="input-user-small" type="text" />
+                </div>
+            </div>
+
+            <div className="divSpaceBetween2">
+                <div className="div-input-user-long">
+                    <p className="bigTitleUserProfile">Descrição</p>
+                    <input className="input-user-long" type="text" />
+                </div>
+            </div>
+
+            <div className="divSpaceBetween2">
+                <div className="div-input-user-long">
+                    <p className="bigTitleUserProfile">Tags</p>
+                    <input className="input-user-long" type="text" />
+                </div>
+            </div>
+
+            <div className="divSpaceBetween2">
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">Email</p>
+                        <input className="input-user-small" type="text" />
+                    </div>
+
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">Nascimento</p>
+                        <input className="input-user-small" type="date" />
+                    </div>
+            </div>
+
+            <div className="divSpaceBetween2">
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">CPF</p>
+                        <input className="input-user-small" type="text" />
+                    </div>
+
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">Telefone</p>
+                        <input className="input-user-small" type="text" />
+                    </div>
+            </div>
+
+            <div className="divSpaceBetween2">
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">CEP</p>
+                        <input className="input-user-small" type="text" />
+                    </div>
+
+                    <div className="div-input-user-small">
+                        <p className="bigTitleUserProfile">Endereço</p>
+                        <input className="input-user-small" type="text" />
+                    </div>
             </div>
         </div>
         </>
